cb39bad5cbc50c4178f83e15757e54620f7b47cf
soleksy <szy.oleksy@gmail.com>
2024-02-02 23:09:05 +0100

    add complete ETL script

--- run_etl.py
+++ run_etl.py
@@ -16,6 +16,11 @@ from etl.transform.transformacts.transform_acts import TransformActs
 
 from etl.common.questionindex.transformed_question_index import TransformedQuestionIndex
 
+from etl.load.loadquestions.load_questions import LoadQuestions
+from etl.load.loadkeywords.load_keywords import LoadKeywords
+from etl.load.loadleafacts.load_leaf_acts import LoadLeafActs
+from etl.load.loadactvectors.load_act_vectors import LoadActVectors
+
 from etl.validate.validate import Validate
 
 
@@ -69,8 +74,6 @@ async def main():
     transform_keywords.transform_not_indexed_keywords()
     logging.info(f'Transformed keywords')
 
-    
-    
     logging.info(f'Transforming acts')
     transform_acts = TransformActs()
     transform_acts.transform_acts()
@@ -80,7 +83,40 @@ async def main():
     validate.validate_dataset()
     logging.info(f'Validation complete')
     '''
+
+    #Loaders
+    '''
+    logging.info(f'Loading questions')
+    load_questions = await LoadQuestions().create()
+    await load_questions.load_questions()
+    logging.info(f'Loaded questions')
+
+    logging.info(f'Validating loaded data')
+    load_questions.validate_loaded_data()
+    logging.info(f'Validation complete')
+
+    logging.info(f'Loading keywords')
+    load_keywords = await LoadKeywords().create()
+    await load_keywords.load_keywords()
+    logging.info(f'Loaded keywords')
+
+    logging.info(f'Loading leaf acts')
+    load_leaf_acts = await LoadLeafActs.create()
+    await load_leaf_acts.load_leaf_acts()
+    await load_leaf_acts.validate_loaded_data()
+    logging.info(f'Loaded leaf acts')
+
+    logging.info(f'Loading act vectors')
+    load_act_vectors = await LoadActVectors.create()
+    await load_act_vectors.load_act_vectors()
+    await load_act_vectors.validate_loaded_data()
+    logging.info(f'Loaded act vectors')
     
+    logging.info(f'Validating loaded data')
+    load_keywords.validate_loaded_data()
+    logging.info(f'Validation complete')
+    '''
+
 
 if __name__ == '__main__':
     asyncio.run(main())
